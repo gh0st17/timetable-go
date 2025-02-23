8a5c5f3d250d3c2841bccc437ebd2d263592eef3
Степан <31482802+gh0st17@users.noreply.github.com>
2025-02-23 09:54:25 +0300

    Update version

--- params/strings.go
+++ params/strings.go
@@ -4,7 +4,7 @@ package params
 
 const (
 	versionDesc = "Печать номера версии и выход"
-	versionText = "timetable 1.0.3b1\n" +
+	versionText = "timetable 1.0.3b2\n" +
 		"Copyright (C) 2025\n" +
 		"Лицензия MIT: THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY\n" +
 		"OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO\n" +
