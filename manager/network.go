14d79ea55c3144a13c3b419f92618e45d4033e14
Степан <31482802+gh0st17@users.noreply.github.com>
2025-03-01 17:17:47 +0300

    Refactor

--- manager/network.go
+++ manager/network.go
@@ -32,10 +32,16 @@ func loadFromUrl(u *url.URL, jar http.CookieJar, proxyUrl *url.URL) (*html.Node,
 	}
 
 	if req, err = http.NewRequest("GET", u.String(), nil); err != nil {
-		return nil, errtype.ErrNetwork(fmt.Errorf("ошибка формирования заголовка запроса: %s", err))
+		return nil, errtype.ErrNetwork(
+			fmt.Errorf("ошибка формирования заголовка запроса: %s", err),
+		)
 	}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_6_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15")
+	req.Header.Set(
+		"User-Agent",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_6_1) "+
+			"AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+	)
 	if resp, err = client.Do(req); err != nil {
 		return nil, errtype.ErrNetwork(fmt.Errorf("ошибка приема данных: %s", err))
 	}
